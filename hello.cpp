a17a250ca3490b22abfcbbab67bfe456959e89a7
tlhasami <talharajpoot2711@gmail.com>
2025-11-16 07:54:55 +0500

    Version1

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    cout << " Line 1" << endl;
+    cout << " Line 2 added" << endl;
+    cout << " Line 3 added" << endl;
+    cout << " Now ammended " << endl;
+    return 0;
+}
